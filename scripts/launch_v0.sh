1bac1f85ca1d7556e913739ee5e76d034fc608ad
Anikait Singh <singh.anikait@gmail.com>
2022-03-03 16:16:45 -0800

    minor script fix

--- scripts/launch_v0.sh
+++ scripts/launch_v0.sh
@@ -1,4 +1,4 @@
-bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_baseline.sh;
-bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline.sh;
-bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline_hotstart.sh;
-bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline_hotstart_weights.sh;
+bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_baseline.sh &
+bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline.sh &
+bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline_hotstart.sh &
+bash /home/asap7772/asap7772/algo_gridworld/scripts/fixedwandb_launch_maze_nobaseline_hotstart_weights.sh &
